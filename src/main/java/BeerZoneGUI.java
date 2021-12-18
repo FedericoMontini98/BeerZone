275d62d7b4b986eae31c741cb1bb7f203b128dbc
ema0498 <ema.tinghi@gmail.com>
2021-12-18 12:36:11 +0100

    Updated register section

--- src/main/java/BeerZoneGUI.java
+++ src/main/java/BeerZoneGUI.java
@@ -1,24 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import static javax.swing.BorderFactory.createEmptyBorder;
 
 public class BeerZoneGUI {
+    private final Integer NUM_REGISTER_ROW = 5;
 
+    private final Color backgroundColor = new Color(255, 170, 3);
     /*
-     * Divisione verticale in due metà
+     * Brewery manager section
      */
-    public static void prepareRegisterSection(JFrame frame){
-        frame.setTitle("BeerZone - REGISTER");
-
+    /*public void BreweryManagerSection(JFrame frame){
+        frame.setTitle("BeerZone - BREWERY MANAGER");
+        frame.setLayout(new GridLayout(1,2));
         JPanel jp = new JPanel();
-        jp.setBackground(new Color(1,255,255));
+        jp.setBorder(BorderFactory.createLineBorder(Color.black));
+        jp.setBackground(backgroundColor);
         frame.getContentPane().add(jp);
 
         jp = new JPanel();
-        jp.setBackground(new Color(255,255,1));
+        jp.setBorder(BorderFactory.createLineBorder(Color.black));
+        jp.setBackground(backgroundColor);
         frame.getContentPane().add(jp);
 
         frame.setVisible(true);
-    }
+    }*/
 
     /*
     *
@@ -72,6 +77,71 @@ public class BeerZoneGUI {
         frame.getContentPane().add(btn, gbc);
     }
 
+    private void prepareRegisterSection(JFrame frame) {
+        frame.setLayout(new GridBagLayout());
+        JPanel jp = new JPanel();
+        GridBagConstraints gbc = new GridBagConstraints();
+        //jp.setBorder(new RoundedBorder(50));
+        frame.getContentPane().add(jp, gbc);
+        jp.setLayout(new GridBagLayout());
+        createInputUserType(jp);
+        createInputField("Username", jp, 1);
+        createInputField("E-mail", jp, 2);
+        createInputField("Password", jp, 3);
+        createInputField("Repeat password", jp, 4);
+        createInputField("Location", jp, 5);
+        JButton registerButton = new JButton("Register");
+        gbc.gridy = 6;
+        gbc.insets = new Insets(0,140,20,0);
+        jp.add(registerButton, gbc);
+        frame.setVisible(true);
+    }
+
+    private void createInputUserType(JPanel panel) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(20, 30, 0, 20);
+        gbc.gridy = 0;
+        gbc.gridx = 0;
+        JTextField description = new JTextField("User type");
+        description.setBorder(createEmptyBorder());
+        description.setEditable(false);
+        panel.add(description, gbc);
+        String[] choices = {"Standard user", "Brewery manager"};
+        final JComboBox<String> cb = new JComboBox<>(choices);
+        cb.setVisible(true);
+        gbc.insets = new Insets(20, 0, 0, 20);
+        gbc.gridx = 1;
+        panel.add(cb, gbc);
+    }
+
+    private void createInputField(String type, JPanel panel, Integer row) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        if(row == NUM_REGISTER_ROW)
+            gbc.insets = new Insets(20,30,20,20);
+        else
+            gbc.insets = new Insets(20,30,0,20);
+
+        gbc.ipady = 8;
+        gbc.gridx = 0;
+        gbc.gridy = row;
+        JTextField description = new JTextField(type);
+        description.setBorder(createEmptyBorder());
+        description.setEditable(false);
+        panel.add(description, gbc);
+        JTextField inputSection = new JTextField();
+        gbc.gridx = 1;
+        gbc.gridwidth = 2;
+        gbc.ipadx = 122;
+        if(row == NUM_REGISTER_ROW)
+            gbc.insets = new Insets(20,0,20,30);
+        else
+            gbc.insets = new Insets(20,0,0,30);
+
+        panel.add(inputSection, gbc);
+    }
+
     /*
      *  starting page with login and register buttons
      */
@@ -85,10 +155,36 @@ public class BeerZoneGUI {
         JFrame frame = new JFrame("BeerZone");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
+        frame.getContentPane().setBackground(backgroundColor);
         GridBagLayout lay = new GridBagLayout();
         frame.setLayout(lay);
         prepareLogRegister(frame);
 
         frame.setVisible(true);
     }
+
+    /*private static class RoundedBorder implements Border {
+
+        private int radius;
+
+        RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+        @Override
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+        }
+
+        @Override
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x,y,width-1,height-1,radius,radius);
+        }
+    }*/
 }
+
+

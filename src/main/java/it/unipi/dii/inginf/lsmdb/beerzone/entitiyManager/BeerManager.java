c837d83ef3b797aea031fa23c1aec4b7606bbbe8
FedericoMontini98 <fedemontini98@gmail.com>
2022-01-01 21:19:52 +0100

    Added functionalities to the correct Entities

--- src/main/java/it/unipi/dii/inginf/lsmdb/beerzone/entitiyManager/BeerManager.java
+++ src/main/java/it/unipi/dii/inginf/lsmdb/beerzone/entitiyManager/BeerManager.java
@@ -3,16 +3,20 @@ package it.unipi.dii.inginf.lsmdb.beerzone.entitiyManager;
 import com.mongodb.client.*;
 import it.unipi.dii.inginf.lsmdb.beerzone.entities.Beer;
 import it.unipi.dii.inginf.lsmdb.beerzone.managerDB.MongoManager;
+import it.unipi.dii.inginf.lsmdb.beerzone.managerDB.Neo4jManager;
 import org.bson.Document;
+import org.neo4j.driver.Session;
 
 import java.util.ArrayList;
 
 import static com.mongodb.client.model.Filters.*;
+import static org.neo4j.driver.Values.parameters;
 
 public class BeerManager {
     private Beer beer;
     private MongoManager mongoManager;
     private MongoCollection<Document> beersCollection;
+    private Neo4jManager NeoDBMS;
 
 
     public BeerManager(){
@@ -53,6 +57,57 @@ public class BeerManager {
             e.printStackTrace();
         }
         return beerList;
+    }
+
+
+    /* Function used to add Beer Nodes in the graph, the only property that they have is id which is common
+     *  Both to reviews and beer's files */
+    public boolean AddBeer (String BeerID){
+        try(Session session = NeoDBMS.getDriver().session()){
+            session.run("CREATE (B:Beer{id: $BeerID})",parameters("BeerID",BeerID));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /* ************************************************************************************************************/
+    /* *************************************  Neo4J Section  ******************************************************/
+    /* ************************************************************************************************************/
+
+    /* Function used to add the relationship of "SameStyle" between two beers.
+     *  It's used to suggest beers based on the common styles between the beers that can be suggested and the beers
+     *  in their favorites */
+    public boolean addSameStyle(String firstBeerID,String secondBeerID){
+        try(Session session = NeoDBMS.getDriver().session()){
+            session.run("MATCH\n" +
+                            "(B1:Beer),\n" +
+                            "(B2:Beer)\n " +
+                            "WHERE B1.id = $BeerID AND B2.id = $BeerID \n" +
+                            "CREATE (B1)-[S:SameStyle]->(B2)\n" +
+                            "CREATE (B2)-[S:SameStyle]->(B1)\n",
+                    parameters( "firstBeerID", firstBeerID, "secondBeerID", secondBeerID));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean removeBeer(String beerID){
+        try(Session session = NeoDBMS.getDriver().session()){
+            session.run("MATCH (B {id: $beerID})\n" +
+                            "DETACH DELETE B",
+                    parameters( "beerID", beerID));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     /*
--- src/main/java/it/unipi/dii/inginf/lsmdb/beerzone/entitiyManager/UserManager.java
+++ src/main/java/it/unipi/dii/inginf/lsmdb/beerzone/entitiyManager/UserManager.java
@@ -6,6 +6,7 @@ import com.mongodb.client.result.UpdateResult;
 import it.unipi.dii.inginf.lsmdb.beerzone.entities.Brewery;
 import it.unipi.dii.inginf.lsmdb.beerzone.entities.StandardUser;
 import it.unipi.dii.inginf.lsmdb.beerzone.managerDB.MongoManager;
+import it.unipi.dii.inginf.lsmdb.beerzone.managerDB.Neo4jManager;
 import org.bson.Document;
 
 import static com.mongodb.client.model.Aggregates.*;
@@ -13,11 +14,24 @@ import static com.mongodb.client.model.Accumulators.*;
 import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Updates.*;
+import static org.neo4j.driver.Values.parameters;
+
+import org.neo4j.driver.Record;
+import org.neo4j.driver.Session;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Transaction;
+import org.neo4j.driver.TransactionWork;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class UserManager {
     private final MongoManager mongoManager;
     private MongoCollection<Document> users;
         // users collection include both standard users (type 0) and breweries (type 1)
+    private Neo4jManager NeoDBMS;
 
     public UserManager() {
         mongoManager = MongoManager.getInstance();
@@ -54,4 +68,109 @@ public class UserManager {
         UpdateResult result = users.updateOne(eq("email", email), set("username", newUsername));
     }
 
+    /* ************************************************************************************************************/
+    /* *************************************  Neo4J Section  ******************************************************/
+    /* ************************************************************************************************************/
+
+    /* Function used to add StandardUser Nodes in the graph, the only property that they have is Username which is common
+     *  Both to reviews and User's files */
+    public boolean AddStandardUser(String ID, String Username){
+        try(Session session = NeoDBMS.getDriver().session()){
+            session.run("CREATE (U:User{username: $Username, ID: $id})",parameters("Username",Username,"ID",ID));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /* Function used to add a favorite beer from the users favorites list. To identify a relationship we need the
+     *  Username and the BeerID, this functionality has to be available on a specific beer only if a User hasn't
+     *  it already in its favorites */
+    public boolean addFavorite(String Username, String BeerID) {
+        try (Session session = NeoDBMS.getDriver().session()) {
+            LocalDateTime MyLDTObj = LocalDateTime.now();
+            DateTimeFormatter myFormatObj  = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+            String str = MyLDTObj.format(myFormatObj);
+            session.run("MATCH\n" +
+                            "  (B:Beer),\n" +
+                            "  (U:User)\n" +
+                            "WHERE U.username = $Username AND B.id = $BeerID'\n" +
+                            "CREATE (U)-[F:Favorites{InDate:$Date}]->(B)\n",
+                    parameters("Username", Username, "BeerID", BeerID, "Date", str));
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /* Function used to remove a favorite beer from the users favorites list. To identify a relationship we need the
+     *  Username and the BeerID, this functionality has to be available on a specific beer only if a User has it in its
+     *  favorites */
+    public boolean removeFavorite(String Username, String BeerID){
+        try(Session session = NeoDBMS.getDriver().session()){
+            session.run("MATCH (U:User {username: $Username})-[F:Favorites]-(B:Beer {id: $BeerID}) \n" +
+                            "DELETE F",
+                    parameters( "Username", Username, "BeerID", BeerID));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    /* Function used to add the relationship of 'reviewed' between a beer and a specific User.
+     * This function has to be available only if the beer hasn't been reviewed from this user yet to avoid multiple
+     * reviews from the same user which can lead to inconsistency or fake values of the avg. score */
+    public boolean addReview(String Username, String BeerID){
+        try(Session session = NeoDBMS.getDriver().session()){
+            Date date = new Date();
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yy");
+            String str = formatter.format(date);
+            session.run("MATCH\n" +
+                            "  (B:Beer),\n" +
+                            "  (U:User)\n" +
+                            "WHERE U.username = $Username AND B.id = $BeerID\n" +
+                            "CREATE (U)-[R:Reviewed{InDate:$Date}]->(B)\n",
+                    parameters( "Username", Username, "BeerID", BeerID,"Date", str));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /* Function used to remove the relationship of 'reviewed' between a beer and a specific User.
+     * This function has to be available only if the beer has been reviewed from this user */
+    public boolean removeReview(String Username, String BeerID){
+        try(Session session = NeoDBMS.getDriver().session()){
+            session.run("MATCH (U:User {username: $Username})-[R:Reviewed]-(B:Beer {id: $BeerID}) \n" +
+                            "DELETE R",
+                    parameters( "Username", Username, "BeerID", BeerID));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /* Function used to remove a user from Neo4J graph DB */
+    public boolean removeUser(String ID, String username){
+        try(Session session = NeoDBMS.getDriver().session()){
+            session.run("MATCH (U {username: $username, ID: $ID})\n" +
+                            "DETACH DELETE U",
+                    parameters( "username", username));
+            return true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 }

9393567fb8d6b006ada5d85505a4c0fc8a824f91
FedericoMontini98 <fedemontini98@gmail.com>
2022-01-04 11:55:58 +0100

    Added functionalities to the correct Entities

--- src/main/java/it/unipi/dii/inginf/lsmdb/beerzone/entitiyManager/BeerManager.java
+++ src/main/java/it/unipi/dii/inginf/lsmdb/beerzone/entitiyManager/BeerManager.java
@@ -5,6 +5,7 @@ import it.unipi.dii.inginf.lsmdb.beerzone.entities.Beer;
 import it.unipi.dii.inginf.lsmdb.beerzone.managerDB.MongoManager;
 import it.unipi.dii.inginf.lsmdb.beerzone.managerDB.Neo4jManager;
 import org.bson.Document;
+import org.neo4j.driver.Result;
 import org.neo4j.driver.Session;
 
 import java.util.ArrayList;
@@ -16,12 +17,13 @@ public class BeerManager {
     private Beer beer;
     private MongoManager mongoManager;
     private MongoCollection<Document> beersCollection;
-    private Neo4jManager NeoDBMS;
+    private final Neo4jManager NeoDBMS;
 
 
     public BeerManager(){
         mongoManager = MongoManager.getInstance();
         beersCollection = mongoManager.getCollection("beer");
+        NeoDBMS = Neo4jManager.getInstance();
     }
 
     public BeerManager (Beer beer) {
@@ -59,36 +61,27 @@ public class BeerManager {
         return beerList;
     }
 
-
-    /* Function used to add Beer Nodes in the graph, the only property that they have is id which is common
-     *  Both to reviews and beer's files */
-    public boolean AddBeer (String BeerID){
-        try(Session session = NeoDBMS.getDriver().session()){
-            session.run("CREATE (B:Beer{id: $BeerID})",parameters("BeerID",BeerID));
-            return true;
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            return false;
-        }
-    }
-
     /* ************************************************************************************************************/
     /* *************************************  Neo4J Section  ******************************************************/
     /* ************************************************************************************************************/
 
-    /* Function used to add the relationship of "SameStyle" between two beers.
-     *  It's used to suggest beers based on the common styles between the beers that can be suggested and the beers
-     *  in their favorites */
-    public boolean addSameStyle(String firstBeerID,String secondBeerID){
+
+
+    /* Function used to add Beer Nodes in the graph, the only property that they have is id which is common
+     *  Both to reviews and beer's files */
+    public boolean AddBeer (String BeerID,String style,String name){
         try(Session session = NeoDBMS.getDriver().session()){
+            //I First have to see if the style node for this beer is already in the graph
+            session.run("MERGE (S:Style{nameStyle: $Style})",parameters("BeerID",BeerID));
+            //I then create the node for the new beer
+            session.run("CREATE (B:Beer{ID: $BeerID,Name: $name,Style: $style})",parameters("BeerID",BeerID,"Name",name,"Style",style));
+            //I create the relationship between the style node and the beer node
             session.run("MATCH\n" +
-                            "(B1:Beer),\n" +
-                            "(B2:Beer)\n " +
-                            "WHERE B1.id = $BeerID AND B2.id = $BeerID \n" +
-                            "CREATE (B1)-[S:SameStyle]->(B2)\n" +
-                            "CREATE (B2)-[S:SameStyle]->(B1)\n",
-                    parameters( "firstBeerID", firstBeerID, "secondBeerID", secondBeerID));
+                            "(B:Beer),\n" +
+                            "(S:Style)\n " +
+                            "WHERE B.BeerID = $BeerID AND S.styleName = $style \n" +
+                            "CREATE (B)-[Ss:SameStyle]->(S)\n",
+                    parameters( "BeerID", BeerID, "styleName", style));
             return true;
         }
         catch(Exception e){
